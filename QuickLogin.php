35bd0e0b593826b3cb4bbd530ad668b05d5a71c8
redsweater <redsweater@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-08 21:18:10 +0000

    First checkin. Reflects Github repository at https://github.com/danielpunkass/QuickLogin/commit/ea539edef56c4150e485ac6ec99e634878be1c42
    git-svn-id: http://plugins.svn.wordpress.org/quicklogin/trunk@420702 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ QuickLogin.php
@@ -0,0 +1,57 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:  QuickLogin
+Plugin URI:   https://github.com/danielpunkass/QuickLogin
+Version:      1.0
+Description:  Adds a keyboard shortcut to your blog for easily logging in.
+Author:       Daniel Jalkut, Red Sweater Software
+Author URI:   http://www.red-sweater.com/blog/
+
+**************************************************************************/
+
+// If you want to use another keystroke besides ESC, set it here
+$triggerKeyCode = 27;
+
+// We depend upon get-current-user type functions, which are not defined by default
+// until AFTER the plugin is given a chance to override.
+require_once(ABSPATH . WPINC . '/pluggable.php');
+
+// We depend upon jQuery that is bundled with WordPress, for easy keystroke detection
+wp_enqueue_script("jquery"); 
+
+function insertQuickLoginTrigger() {
+
+	// When the user "logs in" we send them to the appropriate login page URL, redirecting to current URL
+	$loginPageURL = wp_login_url(get_permalink());
+	global $triggerKeyCode;
+	
+	echo <<<TRIGGEREND
+	<script type="text/javascript">
+	<!-- QuickLogin by Red Sweater Software
+
+	var triggerKeyCode = $triggerKeyCode;
+
+	jQuery(document).keyup(function(e) {
+		if (e.keyCode == triggerKeyCode) {
+			promptForWordPressLogin();
+		}
+	});
+
+	function promptForWordPressLogin() {
+		document.location.href="$loginPageURL";
+	}
+
+	-->
+	</script>
+TRIGGEREND;
+}
+
+$isLoggedIn = is_user_logged_in();
+
+if ($isLoggedIn == False) {
+	add_action('wp_head', 'insertQuickLoginTrigger');
+}
+
+?>
