fc84fb6d6f2ad9a384568c96476f44e1e38bb733
Sam Margulies <freebuffalo@gmail.com>
2011-08-08 16:22:28 -0300

    Rewrite to avoid fatal errors and prevent jquery from always loading

--- QuickLogin.php
+++ QuickLogin.php
@@ -11,25 +11,32 @@ Author URI:   http://www.red-sweater.com/blog/
 
 **************************************************************************/
 
-// If you want to use another keystroke besides ESC, set it here
-$triggerKeyCode = 27;
+function enqueueQuickLoginScripts() {
 
-// We depend upon get-current-user type functions, which are not defined by default
-// until AFTER the plugin is given a chance to override.
-require_once(ABSPATH . WPINC . '/pluggable.php');
-
-// We depend upon jQuery that is bundled with WordPress, for easy keystroke detection
-wp_enqueue_script("jquery"); 
+	if( is_user_logged_in() ) {
+		return;
+	}
+	
+	// We depend upon jQuery that is bundled with WordPress, for easy keystroke detection
+	wp_enqueue_script("jquery"); 
+}
+add_action('init', 'enqueueQuickLoginScripts');
 
 function insertQuickLoginTrigger() {
 
+	if( is_user_logged_in() ) {
+		return;
+	}
+	
+	// If you want to use another keystroke besides ESC, set it here
+	$triggerKeyCode = 27;
+
 	// When the user "logs in" we send them to the appropriate login page URL, redirecting to current URL
 	$loginPageURL = wp_login_url(get_permalink());
-	global $triggerKeyCode;
 	
 	echo <<<TRIGGEREND
 	<script type="text/javascript">
-	<!-- QuickLogin by Red Sweater Software
+	//QuickLogin by Red Sweater Software
 
 	var triggerKeyCode = $triggerKeyCode;
 
@@ -43,15 +50,9 @@ function insertQuickLoginTrigger() {
 		document.location.href="$loginPageURL";
 	}
 
-	-->
 	</script>
 TRIGGEREND;
 }
-
-$isLoggedIn = is_user_logged_in();
-
-if ($isLoggedIn == False) {
-	add_action('wp_head', 'insertQuickLoginTrigger');
-}
+add_action('wp_head', 'insertQuickLoginTrigger');
 
 ?>
